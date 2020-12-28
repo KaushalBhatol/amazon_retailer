be47eee432b325e9e64ec97275e0033bf9efdee7
KaushalBhatol <kaushal.bhatol@outlook.com>
2020-12-28 09:54:41 +0530

    ignore

--- index.php
+++ index.php
@@ -11,8 +11,7 @@
             <!-- Start: Intro -->
             <div class="intro">
                 <h2 class="text-center">Get Top Deals With US!</h2>
-                <p class="text-left">get exclusive offers on amazon.in, top deals are available hear.&nbsp;all products are available on palanpur location.&nbsp;<br>to get top deals go to Top Deals option.<br><br>We provide you amazon palanpur delivery centres also, click
-                    below to get delivery centre locations.&nbsp;<br></p>
+                <p class="text-left">get exclusive offers on amazon.in, top deals are available hear.&nbsp;all products are available on palanpur location.&nbsp;<br>to get top deals go to Top Deals option.<br><br>We provide you amazon palanpur delivery centres also, click below to get delivery centre locations.&nbsp;<br></p>
             </div>
             <!-- End: Intro -->
             <!-- Start: Buttons -->
