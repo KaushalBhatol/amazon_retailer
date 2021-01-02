75224c8ee16281d6955c0ee0b5e708441decb53f
kaushal bhatol <kaushal.bhatol@icloud.com>
2021-01-02 22:37:49 +0530

    v1.2

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ function openTab(evt, tabName) {
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " activeTab";
+  evt.currentTarget.className += " active";
 } 
 
     document.getElementById("defaultOpen").click();
---
+++ page/Top_Deals/page.tab.php
@@ -0,0 +1,3 @@
+
+<div id="TD1"class="tabcontent"><?php include 'page/Top_Deals/1.php'; ?></div>
+<div id="TD2"class="tabcontent"><?php include 'page/Top_Deals/2.php'; ?></div>
